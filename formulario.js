b40e05ef81ef9ebdc9d885066ac9b3e553033a9b
Eduardo Luciano <116396472+EduPaes2005@users.noreply.github.com>
2025-05-16 20:52:49 -0300

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,14 @@
+function Enviar(){
+    let user = 'Bernardo';
+    const alerted = 'Você está na página de contato'
+    let year = '2025';
+
+    if (user === year) {
+        alert('Dado inválido!')
+    }
+
+    if (user == null) {
+    }
+
+    alert(alerted + ' de ' + user + ' em ' + year)
+}
